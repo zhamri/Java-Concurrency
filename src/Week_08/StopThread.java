fe5345db4fbb291da1b1c40589c5b4aeecbb7344
Shanthini21 <31915734+Shanthini21@users.noreply.github.com>
2017-11-05 14:59:22 +0800

    Stop thread

---
+++ src/Week_08/StopThread.java
@@ -0,0 +1,36 @@
+class MyThread extends Thread
+{    
+    @Override
+    public void run()
+    {
+        while (!Thread.interrupted())
+        {
+            System.out.println("Running....");
+        }
+         
+        System.out.println("Stopped Running.....");
+    }
+}
+ 
+public class StopThread
+{   
+    public static void main(String[] args) 
+    {
+        MyThread thread = new MyThread();
+         
+        thread.start();
+         
+        try
+        {
+            Thread.sleep(1000);
+        } 
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+         
+        //interrupting the thread
+         
+        thread.interrupt();
+    }    
+}
