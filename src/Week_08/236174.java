ebd5a9bc1decf34b0d3694bf5b70be835d1a6d86
Aman748 <984184589@qq.com>
2017-11-05 15:17:49 +0800

    Add files via upload

---
+++ src/Week_08/236174.java
@@ -0,0 +1,31 @@
+package Week08_01;
+
+import static java.lang.Thread.currentThread;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Aman
+ */
+public class StopThread {
+    public static void main(String args[]) throws InterruptedException { 
+        Server myServer = new Server(); Thread test = new Thread(myServer, "Test"); 
+        test.start();
+        System.out.println(currentThread().getName() + " is stopping Server thread"); 
+        myServer.stop(); 
+        TimeUnit.MILLISECONDS.sleep(20000);
+        System.out.println(currentThread().getName() + " is finished now"); 
+    }
+}
+class Server implements Runnable{ 
+    private volatile boolean exit = false; 
+    @Override
+    public void run() { 
+        while(!exit){ 
+        System.out.println("Server is running....."); 
+        } 
+    System.out.println("Server is stopped...."); 
+    } 
+    public void stop(){ exit = true; 
+    } 
+}
