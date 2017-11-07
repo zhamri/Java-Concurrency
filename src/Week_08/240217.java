0efcf74150306344de797e22cb25ae90e0818bdd
shiausee <31919477+shiausee@users.noreply.github.com>
2017-11-07 17:38:40 +0800

    Create 240217.java

---
+++ src/Week_08/240217.java
@@ -0,0 +1,38 @@
+
+import static java.lang.Thread.currentThread;
+
+import java.util.concurrent.TimeUnit;
+
+public class Stop_Thread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Server myServer = new Server();
+        Thread t1 = new Thread(myServer, "T1");
+        
+        t1.start();
+        
+        System.out.println(currentThread().getName() + " is stopping Server thread");
+        myServer.stop();
+        
+        TimeUnit.MILLISECONDS.sleep(200);
+        System.out.println(currentThread().getName() + " is finished now");
+    }
+}
+
+class Server implements Runnable {
+
+    private volatile boolean exit = false;
+
+    @Override
+    public void run() {
+        while (!exit) {
+
+            System.out.println("Server is running....");
+        }
+        System.out.println("Server is stopped...");
+    }
+
+    public void stop() {
+        exit = true;
+    }
+}
